43574d578995b23e7a037b75e919914a28bc8e21
XardoX <jurek.gab.jg@gmail.com>
2020-12-12 19:27:18 +0100

    Revert "Merge branch 'assets' into developing"
    This reverts commit 0be60bb9aabfde0484385692977fefa7ee4a98f3, reversing
    changes made to 735a0200d18afadb47bf0bd978eb32d264f67682.

---
+++ Assets/_Scripts/MapGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public GameObject tilePrefab;
+    public Vector2 mapSize;
+    public float tileDistance;
+    void Start()
+    {
+        GenerateMap();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void GenerateMap()
+    {
+        Vector3 nextSpawn = new Vector3(mapSize.x * -tileDistance/2 + tileDistance/2, mapSize.y * -tileDistance/2 + tileDistance/2, 0f);
+        for(int width = 0; width < mapSize.x; width++)
+        {
+            for(int height = 0; height < mapSize.y; height++)
+            {
+                Instantiate(tilePrefab, nextSpawn, transform.rotation, transform);
+                nextSpawn += new Vector3(0f, tileDistance, 0f);
+            }
+            nextSpawn += new Vector3(tileDistance, -tileDistance * mapSize.y, 0f);
+        }
+    }
+}
