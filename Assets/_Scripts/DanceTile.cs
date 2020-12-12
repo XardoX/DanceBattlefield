21990251ec25a114cbc06e7c68e8bc4b9c70a30f
XardoX <jurek.gab.jg@gmail.com>
2020-12-12 11:52:21 +0100

    Basic tile color change

--- Assets/_Scripts/DanceTile.cs
+++ Assets/_Scripts/DanceTile.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class DanceTile : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float colorCooldown;
+    private SpriteRenderer _renderer;
+    private float _colorTime;
+
     void Start()
     {
-        
+        _renderer = GetComponent<SpriteRenderer>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _colorTime -= Time.deltaTime;
+        if(_colorTime < 0)
+        {
+            Color _color = GameManager.instance.tilesColors[Random.Range(0, GameManager.instance.tilesColors.Length)];
+            _renderer.DOColor(_color, colorCooldown);
+            _colorTime = colorCooldown;
+        }
     }
 }
--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -6,6 +6,8 @@ public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
     public static GameManager instance;
+
+    public Color[] tilesColors;
     private void Awake() 
     {
         if(instance == null)
